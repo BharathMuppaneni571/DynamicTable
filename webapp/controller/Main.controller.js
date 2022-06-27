8dfae7c74d385ad9672a4861d21d343fbedead9c
BharathMuppaneni571 <muppanenibharath571@gmail.com>
2022-06-27 11:25:35 +0000

    save implementation demo

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -15,110 +15,113 @@ sap.ui.define([
                         { "name": "Categories" },
                         { "name": "Employees" }
                     ]
-                    , "rowData": {"Categories": [
-                        {   "CategoryID": 1,
-                            "CategoryName": "Beverages",
-                            "Description": "Soft drinks, coffees, teas, beers, and ales"
-                        },
-                        {
-                            "CategoryID": 2,
-                            "CategoryName": "Condiments",
-                            "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
-                        },
-                        {
-                            "CategoryID": 3,
-                            "CategoryName": "Confections",
-                            "Description": "Desserts, candies, and sweet breads"
-                        },
-                        {
-                            "CategoryID": 4,
-                            "CategoryName": "Dairy Products",
-                            "Description": "Cheeses"
-                        },
-                        {
-                            "CategoryID": 5,
-                            "CategoryName": "Grains/Cereals",
-                            "Description": "Breads, crackers, pasta, and cereal"
-                        },
-                        {
-                            "CategoryID": 6,
-                            "CategoryName": "Meat/Poultry",
-                            "Description": "Prepared meats"
-                        },
-                        {
-                            "CategoryID": 7,
-                            "CategoryName": "Produce",
-                            "Description": "Dried fruit and bean curd"
-                        },
-                        {
-                            "CategoryID": 8,
-                            "CategoryName": "Seafood",
-                            "Description": "Seaweed and fish"
-                        }
-                    ],
-                    "Employees": [
-                        {   "EmployeeID": 1,
-                            "LastName": "Davolio",
-                            "FirstName": "Nancy",
-                            "Title": "Sales Representative"
-                        },
-                        {
-                            "EmployeeID": 2,
-                            "LastName": "Fuller",
-                            "FirstName": "Andrew",
-                            "Title": "Vice President, Sales"
-                        },
-                        {
-                            "EmployeeID": 3,
-                            "LastName": "Leverling",
-                            "FirstName": "Janet",
-                            "Title": "Sales Representative"
-                        },
-                        {
-                            "EmployeeID": 4,
-                            "LastName": "Peacock",
-                            "FirstName": "Margaret",
-                            "Title": "Sales Representative"
-                        },
-                        {
-                            "EmployeeID": 5,
-                            "LastName": "Buchanan",
-                            "FirstName": "Steven",
-                            "Title": "Sales Manager"
-                        },
-                        {
-                            "EmployeeID": 6,
-                            "LastName": "Suyama",
-                            "FirstName": "Michael",
-                            "Title": "Sales Representative"
-                        },
-                        {
-                            "EmployeeID": 7,
-                            "LastName": "King",
-                            "FirstName": "Robert",
-                            "Title": "Sales Representative"
-                        },
-                        {
-                            "EmployeeID": 8,
-                            "LastName": "Callahan",
-                            "FirstName": "Laura",
-                            "Title": "Inside Sales Coordinator"
-                        },
-                        {
-                            "EmployeeID": 9,
-                            "LastName": "Dodsworth",
-                            "FirstName": "Anne",
-                            "Title": "Sales Representative"
-                        }
-                    ]
-                }
+                    , "rowData": {
+                        "Categories": [
+                            {
+                                "CategoryID": 1,
+                                "CategoryName": "Beverages",
+                                "Description": "Soft drinks, coffees, teas, beers, and ales"
+                            },
+                            {
+                                "CategoryID": 2,
+                                "CategoryName": "Condiments",
+                                "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+                            },
+                            {
+                                "CategoryID": 3,
+                                "CategoryName": "Confections",
+                                "Description": "Desserts, candies, and sweet breads"
+                            },
+                            {
+                                "CategoryID": 4,
+                                "CategoryName": "Dairy Products",
+                                "Description": "Cheeses"
+                            },
+                            {
+                                "CategoryID": 5,
+                                "CategoryName": "Grains/Cereals",
+                                "Description": "Breads, crackers, pasta, and cereal"
+                            },
+                            {
+                                "CategoryID": 6,
+                                "CategoryName": "Meat/Poultry",
+                                "Description": "Prepared meats"
+                            },
+                            {
+                                "CategoryID": 7,
+                                "CategoryName": "Produce",
+                                "Description": "Dried fruit and bean curd"
+                            },
+                            {
+                                "CategoryID": 8,
+                                "CategoryName": "Seafood",
+                                "Description": "Seaweed and fish"
+                            }
+                        ],
+                        "Employees": [
+                            {
+                                "EmployeeID": 1,
+                                "LastName": "Davolio",
+                                "FirstName": "Nancy",
+                                "Title": "Sales Representative"
+                            },
+                            {
+                                "EmployeeID": 2,
+                                "LastName": "Fuller",
+                                "FirstName": "Andrew",
+                                "Title": "Vice President, Sales"
+                            },
+                            {
+                                "EmployeeID": 3,
+                                "LastName": "Leverling",
+                                "FirstName": "Janet",
+                                "Title": "Sales Representative"
+                            },
+                            {
+                                "EmployeeID": 4,
+                                "LastName": "Peacock",
+                                "FirstName": "Margaret",
+                                "Title": "Sales Representative"
+                            },
+                            {
+                                "EmployeeID": 5,
+                                "LastName": "Buchanan",
+                                "FirstName": "Steven",
+                                "Title": "Sales Manager"
+                            },
+                            {
+                                "EmployeeID": 6,
+                                "LastName": "Suyama",
+                                "FirstName": "Michael",
+                                "Title": "Sales Representative"
+                            },
+                            {
+                                "EmployeeID": 7,
+                                "LastName": "King",
+                                "FirstName": "Robert",
+                                "Title": "Sales Representative"
+                            },
+                            {
+                                "EmployeeID": 8,
+                                "LastName": "Callahan",
+                                "FirstName": "Laura",
+                                "Title": "Inside Sales Coordinator"
+                            },
+                            {
+                                "EmployeeID": 9,
+                                "LastName": "Dodsworth",
+                                "FirstName": "Anne",
+                                "Title": "Sales Representative"
+                            }
+                        ]
+                    }
 
                 };
 
 
                 var oModel = new sap.ui.model.json.JSONModel();
                 oModel.setData(data);
-                this.getView().setModel(oModel,"sampleModel");
+                this.getView().setModel(oModel, "sampleModel");
 
             },
             onSelectionChange: function (oEvent) {
@@ -130,19 +133,19 @@ sap.ui.define([
 
 
                 var rowData = [];
-                temprowData.forEach(function(item){ 
+                temprowData.forEach(function (item) {
                     rowData.push(item);
                 });
-                 var oModel = new sap.ui.model.json.JSONModel();
+                var oModel = new sap.ui.model.json.JSONModel();
                 oModel.setData({
-                rows: rowData,
-                columns: columnData
+                    rows: rowData,
+                    columns: columnData
                 });
                 var oTable = this.byId("idDynamictable");
                 oTable.setModel(oModel);
 
 
-                oTable.bindColumns("/columns", function(sId, oContext) {
+                oTable.bindColumns("/columns", function (sId, oContext) {
                     var columnName = oContext.getObject();
                     return new sap.ui.table.Column({
                         label: columnName,
@@ -152,72 +155,99 @@ sap.ui.define([
 
                 oTable.bindRows("/rows");
             },
-            onItemSave: function(){
+            onItemSave: function () {
                 debugger;
             },
-            onItemEdit: function(){
+            onItemEdit: function () {
                 if (!this.onEditFrag) {
-                    this.onEditFrag = sap.ui.xmlfragment("com.testing.dynamictable.view.edit",this);
+                    this.onEditFrag = sap.ui.xmlfragment("com.testing.dynamictable.view.edit", this);
                     this.getView().addDependent(this.onEditFrag);
 
-                //getdata
-                var selectedIndex = this.byId("idDynamictable").getSelectedIndices();
-                var item = this.byId("idDynamictable").getModel().getData().rows[selectedIndex];
-                this.obbeforefragopen(item);
-                this.onEditFrag.open();
-            }else{
-                //getdata
-                var selectedIndex = this.byId("idDynamictable").getSelectedIndices();
-                var item = this.byId("idDynamictable").getModel().getData().rows[selectedIndex];
-                this.obbeforefragopen(item);
-                this.onEditFrag.open();
-            }
-        },
-
-        obbeforefragopen: function(item){
-            var oForm = sap.ui.getCore().byId('SimpleFormChange354');        
-            // addig items
-            oForm.removeAllContent();
-
-                for(var i= 0 ;i< Object.keys(item).length; i++){
-                    oForm.addContent( new sap.m.Label({ text: Object.keys(item)[i] }) );
-                    if(Object.keys(item)[i] == "Title" || Object.keys(item)[i] == "Description"){
-                        oForm.addContent( new sap.m.Input({ type: sap.m.InputType.Text, value: Object.values(item)[i],editable: true }) );        
-                        }else{
-                            oForm.addContent( new sap.m.Input({ type: sap.m.InputType.Text, value: Object.values(item)[i],editable: false }) );        
-                    } 
+                    //getdata
+                    var selectedIndex = this.byId("idDynamictable").getSelectedIndices();
+                    var item = this.byId("idDynamictable").getModel().getData().rows[selectedIndex];
+                    this.obbeforefragopen(item);
+                    this.onEditFrag.open();
+                } else {
+                    //getdata
+                    var selectedIndex = this.byId("idDynamictable").getSelectedIndices();
+                    var item = this.byId("idDynamictable").getModel().getData().rows[selectedIndex];
+                    this.obbeforefragopen(item);
+                    this.onEditFrag.open();
                 }
             },
 
-            onupdateDB: function(){
+            obbeforefragopen: function (item) {
+                var oForm = sap.ui.getCore().byId('SimpleFormChange354');
+                // addig items
+                oForm.removeAllContent();
+
+                for (var i = 0; i < Object.keys(item).length; i++) {
+                    oForm.addContent(new sap.m.Label({ text: Object.keys(item)[i] }));
+                    if (Object.keys(item)[i] == "Title" || Object.keys(item)[i] == "Description") {
+                        oForm.addContent(new sap.m.Input({ type: sap.m.InputType.Text, value: Object.values(item)[i], editable: true }));
+                    } else {
+                        oForm.addContent(new sap.m.Input({ type: sap.m.InputType.Text, value: Object.values(item)[i], editable: false }));
+                    }
+                }
+            },
+
+            onupdateDB: function () {
                 debugger;
                 //geting values
                 var oForm = sap.ui.getCore().byId('SimpleFormChange354');
 
                 var pnlDom = oForm.getDomRef()
-                var lables= [];
-                $(pnlDom).find('label').each(function(index, elem) {
-                    lables.push($(elem).text());          
+                var lables = [];
+                $(pnlDom).find('label').each(function (index, elem) {
+                    lables.push($(elem).text());
                 });
 
-                var values = []; 
-                $(pnlDom).find('input').each(function(index, elem) {
-                    values.push($(elem)[0].value);          
+                var values = [];
+                $(pnlDom).find('input').each(function (index, elem) {
+                    values.push($(elem)[0].value);
                 });
-                
+
                 // var payload = {};
                 var tmpObj = {};
-                lables.forEach(function(lable,index){
+                lables.forEach(function (lable, index) {
                     tmpObj[lable] = values[index];
                 })
-                
+
                 var payload = {
                     "id": tmpObj.CategoryID
                 }
                 
-                this.onEditFrag.close();
+                // start of save
+                sap.ui.core.BusyIndicator.show();
+                var oToken = this.getOwnerComponent()._csrfToken;
+                var sUrl = "xyzxyz/xyzxyz.xsjs ......payload-";
+
+                $.ajax({
+                    url: sUrl,
+                    type: "POST",
+                    data: JSON.stringify(payload),
+                    dataType: "json",
+                    contentType: "application/json",
+                    headers: {
+                        "X-CSRF-Token": oToken
+                    },
+                    success: function (data) {
+                        // success
+                        sap.ui.core.BusyIndicator.hide();
+                        this.onEditFrag.close();
+
+                    },
+                    error: function (err) {
+                        // error
+                        sap.ui.core.BusyIndicator.hide();
+                        MessageBox.error(err.statusCode);
+                        this.onEditFrag.close();
+                    }
+                });
+                //  end of save
             },
-            closeDialog: function(){
+            closeDialog: function () {
                 this.onEditFrag.close();
             }
 
